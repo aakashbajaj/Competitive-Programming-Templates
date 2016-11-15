0892292795e77c1cae51d9ca9719c247abdbd32c
aakash2007 <f2015586@pilani.bits-pilani.ac.in>
2016-11-15 16:15:16 +0530

    Initial Commit

---
+++ C++Template.cpp
@@ -0,0 +1,48 @@
+#include <cstdio>
+#include <cmath>
+#include <iostream>
+#include <set>
+#include <algorithm>
+#include <vector>
+#include <map>
+#include <cassert>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <limits.h>
+#include <string.h>
+ 
+using namespace std;
+ 
+#define rep(i,a,b)  for(int i=a; a<b; i++)
+#define strlp(s,i,a)	for(int i=a;s[i]!='\0';i++)
+#define all(v)  v.begin(),v.end()
+ 
+typedef long long int LL;
+typedef pair<int, int > pii;
+typedef vector<int > vi;
+ 
+inline void scn(int &a)
+{
+  char k = 0;
+  while(k<33)
+ 
+  k=getc(stdin);
+  a=0;
+  while(k>33)
+  {
+    a = a*10 + k - '0';
+    k = getc(stdin);
+  }
+}
+
+int main()
+{
+	int t;
+	scn(t);
+	while(t--)
+	{
+
+	}
+	return 0;
+}
